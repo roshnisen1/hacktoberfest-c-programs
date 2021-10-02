c1380ffbc6e8e39f371cecb28ab2e4856c36af0c
Sohil Maurya <68151294+sss1stclass@users.noreply.github.com>
2021-10-02 14:12:26 +0530

    Update even-odd.c

--- even-odd.c
+++ even-odd.c
@@ -2,11 +2,11 @@
 
 int main(){
   int a=0;   // taking a= 0
-  scanf("%d",&a); // for geeting the values from user
+  scanf("%d",&a); // for geting the values from user
   if(a%2==0)
     printf("Even"); // for printing the output
   else
     printf("Odd");  // // for printing the output
   
-return 0;
+return 0; // returning the main function as 0
 } 
